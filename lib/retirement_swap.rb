9227efe40fbfeb1cfa956dc39f981d32f1fd1c3c
Marten Veldthuis <marten@veldthuis.com>
2015-06-30 16:23:31 +0200

    Add rakefile for travis

--- lib/retirement_swap.rb
+++ lib/retirement_swap.rb
@@ -1,6 +1,6 @@
 require_relative 'retirement_swap/kafka_queue_consumer'
 require_relative 'retirement_swap/storage/memory'
-require_relative 'retirement_swap/simple_retirement'
+require_relative 'retirement_swap/swap_algorithm'
 
 module RetirementSwap
 end
