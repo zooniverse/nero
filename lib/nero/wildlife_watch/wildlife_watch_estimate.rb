e6711caadec7d2421b51a86167233ea3fbee9ca2
Marten Veldthuis <marten@veldthuis.com>
2015-11-26 14:48:44 +0000

    Fix alignment

--- lib/nero/wildlife_watch/wildlife_watch_estimate.rb
+++ lib/nero/wildlife_watch/wildlife_watch_estimate.rb
@@ -17,9 +17,9 @@ module Nero
         grouped_votes = votes.group_by { |i| i }
         grouped_votes.default = []
 
-        return :human       if grouped_votes["human"].size >= 1                    # if one classification says it's a human
-        return :five_blanks if grouped_votes["blank"].size >= 5                    # if 5 classifications say the image is blank
-        return :consensus   if grouped_votes.any? { |_, group| group.size >= 7 } # if 7 classifications agree
+        return :human        if grouped_votes["human"].size >= 1                    # if one classification says it's a human
+        return :five_blanks  if grouped_votes["blank"].size >= 5                    # if 5 classifications say the image is blank
+        return :consensus    if grouped_votes.any? { |_, group| group.size >= 7 } # if 7 classifications agree
         return :three_blanks if votes[0..2].count("blank") == 3                    # if the first 3 classifications say the image is blank
         false # leaving the 15 classifications limit up to panoptes
       end
