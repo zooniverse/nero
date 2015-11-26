64986508bac89117f83d919d43e0de1a87756f0f
Marten Veldthuis <marten@veldthuis.com>
2015-11-26 14:59:13 +0000

    Refactor Wildlife Watch Estimate vote counting

--- lib/nero/wildlife_watch/wildlife_watch_estimate.rb
+++ lib/nero/wildlife_watch/wildlife_watch_estimate.rb
@@ -4,25 +4,34 @@ require 'set'
 module Nero
   module WildlifeWatch
     class WildlifeWatchEstimate < SimpleDelegator
+      def add_vote(vote)
+        data["votes"] ||= []
+        data["votes"] << vote
+        @vote_counts = nil
+      end
+
+      def retired?
+        return :human        if vote_counts["human"] >= 1                    # if one classification says it's a human
+        return :five_blanks  if vote_counts["blank"] >= 5                    # if 5 classifications say the image is blank
+        return :consensus    if vote_counts.any? { |_, count| count >= 7 }   # if 7 classifications agree
+        return :three_blanks if votes[0..2].count("blank") == 3              # if the first 3 classifications say the image is blank
+        false # leaving the 15 classifications limit up to panoptes
+      end
+
+      private
+
       def votes
         data["votes"] ||= []
         data["votes"]
       end
 
-      def add_vote(vote)
-        self.votes << vote
+      def vote_counts
+        @vote_counts ||= votes.reduce(Hash.new(0)) do |groups, vote|
+          groups[vote] += 1
+          groups
+        end
       end
 
-      def retired?
-        grouped_votes = votes.group_by { |i| i }
-        grouped_votes.default = []
-
-        return :human        if grouped_votes["human"].size >= 1                    # if one classification says it's a human
-        return :five_blanks  if grouped_votes["blank"].size >= 5                    # if 5 classifications say the image is blank
-        return :consensus    if grouped_votes.any? { |_, group| group.size >= 7 } # if 7 classifications agree
-        return :three_blanks if votes[0..2].count("blank") == 3                    # if the first 3 classifications say the image is blank
-        false # leaving the 15 classifications limit up to panoptes
-      end
     end
   end
 end
--- spec/nero/wildlife_watch/wildlife_watch_estimate_spec.rb
+++ spec/nero/wildlife_watch/wildlife_watch_estimate_spec.rb
@@ -5,46 +5,46 @@ describe Nero::WildlifeWatch::WildlifeWatchEstimate do
 
   describe '#retired?' do
     it "is retired when any classification says it's a human" do
-      estimate.votes << "cow"
-      estimate.votes << "cow"
-      expect { estimate.votes << "human" }.to change { estimate.retired? }.from(false).to(:human)
+      estimate.add_vote "cow"
+      estimate.add_vote "cow"
+      expect { estimate.add_vote "human" }.to change { estimate.retired? }.from(false).to(:human)
     end
 
     it "is retired when the first three classifications say the image is blank" do
-      estimate.votes << "blank"
-      estimate.votes << "blank"
-      expect { estimate.votes << "blank" }.to change { estimate.retired? }.from(false).to(:three_blanks)
+      estimate.add_vote "blank"
+      estimate.add_vote "blank"
+      expect { estimate.add_vote "blank" }.to change { estimate.retired? }.from(false).to(:three_blanks)
     end
 
     it 'is not retired when one of the first three classifications says the image is not blank' do
-      estimate.votes << "blank"
-      estimate.votes << "cow"
-      estimate.votes << "blank"
+      estimate.add_vote "blank"
+      estimate.add_vote "cow"
+      estimate.add_vote "blank"
       expect(estimate.retired?).to eq(false)
     end
 
     it "is retired when five classifications say the image is blank" do
-      estimate.votes << "blank"
-      estimate.votes << "cow"
-      estimate.votes << "blank"
-      estimate.votes << "cow"
-      estimate.votes << "blank"
-      estimate.votes << "cow"
-      estimate.votes << "blank"
-      estimate.votes << "cow"
-      expect { estimate.votes << "blank" }.to change { estimate.retired? }.from(false).to(:five_blanks)
+      estimate.add_vote "blank"
+      estimate.add_vote "cow"
+      estimate.add_vote "blank"
+      estimate.add_vote "cow"
+      estimate.add_vote "blank"
+      estimate.add_vote "cow"
+      estimate.add_vote "blank"
+      estimate.add_vote "cow"
+      expect { estimate.add_vote "blank" }.to change { estimate.retired? }.from(false).to(:five_blanks)
     end
 
     it "is retired when seven classifications agree" do
-      estimate.votes << "cow"
-      estimate.votes << "cow"
-      estimate.votes << "duck"
-      estimate.votes << "cow"
-      estimate.votes << "duck"
-      estimate.votes << "cow"
-      estimate.votes << "cow"
-      estimate.votes << "cow"
-      expect { estimate.votes << "cow" }.to change { estimate.retired? }.from(false).to(:consensus)
+      estimate.add_vote "cow"
+      estimate.add_vote "cow"
+      estimate.add_vote "duck"
+      estimate.add_vote "cow"
+      estimate.add_vote "duck"
+      estimate.add_vote "cow"
+      estimate.add_vote "cow"
+      estimate.add_vote "cow"
+      expect { estimate.add_vote "cow" }.to change { estimate.retired? }.from(false).to(:consensus)
     end
   end
 end
