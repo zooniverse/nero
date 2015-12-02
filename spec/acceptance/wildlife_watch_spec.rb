fe058f6fccf26a7e2f884084d920a862db82bf1d
Marten Veldthuis <marten@veldthuis.com>
2015-12-02 11:35:37 +0000

    Add USA Wildlife Watch acceptance spec

---
+++ spec/acceptance/wildlife_watch_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'Wildlife Watch' do
+  let(:storage) { Nero::Storage.new(DB) }
+  let(:output) { Nero::Output::IOWriter.new(StringIO.new) }
+  let(:processor) { Nero::Processor.new(storage, output, "1021" => {"algorithm" => "wildlife_watch"}) }
+
+  after do
+    verify do
+      DB[:estimates].all.sort_by { |row| row[:subject_id] }
+                        .map { |row| [row[:subject_id], JSON.load(row[:data])] }
+    end
+  end
+
+  context 'with io and sequel' do
+    it 'processes the fixture' do
+      File.open(File.expand_path("../../fixtures/wildlife_watch.json", __FILE__), 'r') do |io|
+        reader = Nero::Input::IOReader.new(io, processor)
+        reader.run
+      end
+    end
+  end
+end
