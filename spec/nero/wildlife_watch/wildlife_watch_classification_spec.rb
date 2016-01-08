d65592fe60fcb94d7e77030e9e8128c176d901f2
Marten Veldthuis <marten@veldthuis.com>
2016-01-08 11:05:34 +0000

    Fix spec

--- spec/nero/wildlife_watch/wildlife_watch_classification_spec.rb
+++ spec/nero/wildlife_watch/wildlife_watch_classification_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
 
 describe Nero::WildlifeWatch::WildlifeWatchClassification do
-  let(:nothing_here)             { [{"task" => "init", "value" => 0}] }
-  let(:i_see_human_nothing_more) { [{"task" => "init", "value" => 1}] }
-  let(:i_see_human_with_deer)    { [{"task" => "init", "value" => 1}, {"task" => "T1", "value" => [{"choice" => "DR", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
-  let(:i_see_human_with_human)   { [{"task" => "init", "value" => 1}, {"task" => "T1", "value" => [{"choice" => "HMN", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
-  let(:i_see_animal_with_deer)   { [{"task" => "init", "value" => 2}, {"task" => "T1", "value" => [{"choice" => "DR", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
-  let(:i_see_animal_with_human)  { [{"task" => "init", "value" => 2}, {"task" => "T1", "value" => [{"choice" => "HMN", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
+  let(:no_answers)    { [] }
+  let(:i_see_nothing) { [{"task" => "T1", "value" => [{"choice"=>"NTHNGHR", "answers"=>{}, "filters"=>{}}]}] }
+  let(:i_see_deer)    { [{"task" => "T1", "value" => [{"choice" => "DR", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
+  let(:i_see_human)   { [{"task" => "T1", "value" => [{"choice" => "HMN", "answers" => {"HWMN" => "1", "NGPRSNT" => "N"}, "filters" => {}}]}] }
 
   def make_classification(annotations)
     base = Nero::Classification.new("annotations" => annotations)
@@ -14,20 +12,20 @@ describe Nero::WildlifeWatch::WildlifeWatchClassification do
   end
 
   describe '#vote' do
-    it 'detects blanks' do
-      expect(make_classification(nothing_here).vote).to eq("blank")
+    it 'detects empty answers' do
+      expect(make_classification(no_answers).vote).to eq("blank")
     end
 
-    it 'detects humans from initial question' do
-      expect(make_classification(i_see_human_nothing_more).vote).to eq("human")
+    it 'detects blanks from classification step' do
+      expect(make_classification(i_see_nothing).vote).to eq("blank")
     end
 
     it 'detects humans from classification step' do
-      expect(make_classification(i_see_animal_with_human).vote).to eq("human")
+      expect(make_classification(i_see_human).vote).to eq("human")
     end
 
     it 'detects animals' do
-      expect(make_classification(i_see_animal_with_deer).vote).to eq("DR")
+      expect(make_classification(i_see_deer).vote).to eq("DR")
     end
   end
 end
