18b94288dfeb5a2c98af6330cd4746b53ba1de69
Marten Veldthuis <marten@veldthuis.com>
2016-04-23 08:01:02 -0500

    Add support for WIWW workflow 1590

--- spec/acceptance/wildlife_watch_spec.rb
+++ spec/acceptance/wildlife_watch_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 describe 'Wildlife Watch' do
   let(:storage) { Nero::Storage.new(DB) }
   let(:output) { Nero::Output::IOWriter.new(StringIO.new) }
-  let(:processor) { Nero::Processor.new(storage, output, "1021" => {"algorithm" => "wildlife_watch"}) }
+  let(:processor) do
+    Nero::Processor.new(storage, output, {
+      "1021" => {"algorithm" => "wildlife_watch"},
+      "1590" => {"algorithm" => "wildlife_watch"}
+    })
+  end
 
   after do
     verify do
@@ -12,12 +17,17 @@ describe 'Wildlife Watch' do
     end
   end
 
-  context 'with io and sequel' do
-    it 'processes the fixture' do
-      File.open(File.expand_path("../../fixtures/wildlife_watch.json", __FILE__), 'r') do |io|
-        reader = Nero::Input::IOReader.new(io, processor)
-        reader.run
-      end
+  it 'processes workflow 1021' do
+    File.open(File.expand_path("../../fixtures/wildlife_watch_workflow_1021.json", __FILE__), 'r') do |io|
+      reader = Nero::Input::IOReader.new(io, processor)
+      reader.run
+    end
+  end
+
+  it 'processes workflow 1590' do
+    File.open(File.expand_path("../../fixtures/wildlife_watch_workflow_1590.json", __FILE__), 'r') do |io|
+      reader = Nero::Input::IOReader.new(io, processor)
+      reader.run
     end
   end
 end
