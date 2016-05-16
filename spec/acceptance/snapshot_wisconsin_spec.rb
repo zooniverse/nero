2fd1fbc99c35cd4958931cbb670ed32b6e66517a
Marten Veldthuis <marten@veldthuis.com>
2016-05-16 11:08:44 +0100

    Add support for snapshot wisconsin (#24)

---
+++ spec/acceptance/snapshot_wisconsin_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'Snapshot Wisconsin' do
+  let(:storage) { Nero::Storage.new(DB) }
+  let(:output) { Nero::Output::IOWriter.new(StringIO.new) }
+  let(:processor) do
+    Nero::Processor.new(storage, output,
+                        "1717" => {"algorithm" => "wildlife_watch"})
+  end
+
+  after do
+    verify do
+      DB[:estimates].all.sort_by { |row| row[:subject_id] }
+                        .map { |row| [row[:subject_id], row[:data]] }
+    end
+  end
+
+  it 'processes' do
+    File.open(File.expand_path("../../fixtures/snapshot_wisconsin.json", __FILE__), 'r') do |io|
+      reader = Nero::Input::IOReader.new(io, processor)
+      reader.run
+    end
+  end
+end
